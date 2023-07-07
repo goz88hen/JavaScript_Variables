7117bbf7ae3864cde58bdd402447174e8d830523
Henry (Hendra) Gozali <123764602+goz88hen@users.noreply.github.com>
2023-07-06 17:54:32 -0700

    Completed the exercise for javascript.

--- app.js
+++ app.js
@@ -3,8 +3,33 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName =  `Henry`;
+let lastname = `Gozali`;
+var age = 28;
 
-// Exercise 2
+lastName = `Contreras`;
+age = 24;
 
-// Exercise 3
+let language = `Javascript`;
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was first release in ${createdYear}.`);
+
+let product = `Floppy Disk`;
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let instock= false;
+let selectSize = "M";
+
+console.log(`${product} in size ${selectSize} is $ ${price} with ${stock} in stock.`);
+
+let title = `Name of the Wind`;
+let author = `Patrick Rothfuss`;
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true; 
+
+console.log(`${title} was written by ${author} since it is ${pageCount} pages long.Your last bookmark was page ${bookmark}.`);
